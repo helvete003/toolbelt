9c563ee7a6096ef0ad22b96b8c4ee9dfa115aea5
helvete003 <d.obieglo@gmail.com>
2022-01-22 00:16:37 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,11 @@ extern {
     pub fn log(str: &str);
 }
 
-macro_rules! console_log {
+/*macro_rules! console_log {
     // Note that this is using the `log` function imported above during
     // `bare_bones`
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
+}*/
 
 #[wasm_bindgen]
 pub enum FileFormat {
